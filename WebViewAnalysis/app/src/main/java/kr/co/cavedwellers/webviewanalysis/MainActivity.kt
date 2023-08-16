7d81915c6be34d34dea26e42f70c40294abb60d8
neander <b3xlon9@gmail.com>
2023-08-16 13:52:29 +0900

    add: setting webview layout

--- WebViewAnalysis/app/src/main/java/kr/co/cavedwellers/webviewanalysis/MainActivity.kt
+++ WebViewAnalysis/app/src/main/java/kr/co/cavedwellers/webviewanalysis/MainActivity.kt
@@ -1,11 +1,26 @@
 package kr.co.cavedwellers.webviewanalysis
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.cavedwellers.webviewanalysis.databinding.ActivityMainBinding
+import android.util.Base64
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        // android guide line
+        // load webview
+        // val caveUrl = "https://portfolio.cavedwellers.co.kr/"
+        val unencodeHtml = "<html><body>'%23' is the percent code for '#' </body></html>"
+        val encodeHtml = Base64.encodeToString(unencodeHtml.toByteArray(), Base64.NO_PADDING)
+        binding.webLayout.loadUrl(encodeHtml)
     }
 }
