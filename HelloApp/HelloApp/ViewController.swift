0d66c69c4fc06a64bd50497821e3529b4cf044a9
neander <b3xlon9@gmail.com>
2023-08-21 21:51:55 +0900

    [doit] helloApp btn send event

--- HelloApp/HelloApp/ViewController.swift
+++ HelloApp/HelloApp/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lbHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnSend(_ sender: UIButton) {
+        lbHello.text = "Hello, " + txtName.text!    // optional
+    }
 }
 
